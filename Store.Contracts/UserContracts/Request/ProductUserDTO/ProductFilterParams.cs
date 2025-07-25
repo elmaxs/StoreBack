4494b06f7adf5aa096cb4d33ba42b7978a4574a1
Yelyseiev Maksym <ytk1488@gmail.com>
2025-07-25 15:50:46 +0300

    start create sort logic

--- Store.Contracts/UserContracts/Request/ProductUserDTO/ProductFilterParams.cs
+++ Store.Contracts/UserContracts/Request/ProductUserDTO/ProductFilterParams.cs
@@ -4,7 +4,8 @@
     {
         public Guid? CategoryId { get; set; }
         public string Order { get; set; } = "asc";
+        public string Sort { get; set; } = "price";
         public int Page { get; set; } = 1;
-        public int PageSize { get; set; } = 4;
+        public int PageSize { get; set; } = 10;
     }
 }
